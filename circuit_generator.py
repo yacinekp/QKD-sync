0e2e4d99bd9c395429fa1357d924879ab3fa30e5
yacinekp <71157269+yacinekp@users.noreply.github.com>
2026-05-16 18:55:12 +0100

    socket communication working

--- circuit_generator.py
+++ circuit_generator.py
@@ -1,91 +1,60 @@
-import random
-import json
-from qiskit import QuantumCircuit, transpile
-from qiskit_aer import AerSimulator
-from datetime import datetime
+from qiskit import QuantumCircuit
 from qiskit.qasm3 import dumps
+from qiskit_aer import AerSimulator
+from qiskit import transpile
+import socket
+from datetime import datetime
+import json
 
-import os
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.fernet import Fernet
+#la fonction de generation du circuit de ref
+def build_quantum_circuit():
+    qc = QuantumCircuit(4,4, name="reference circuit") # 4 qubits pour les 4 etats 0 1 + -
+    # q0 = |0>
+    qc.x(1) #q1 = |1>
+    qc.h(2) #qc2 = |+>
+    qc.x(3)
+    qc.h(3) # qc3 = |->
+    return qc
 
-n_qubits = 10
-tmps_creation= datetime.now().strftime("%d/%m/%Y_%H:%M:%S")
-id_session =datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+#alice genere ce circuit :
+ref_qc = build_quantum_circuit()
+qasm_circuit = dumps(ref_qc)
 
-shared_bits = [random.randint(0, 1) for _ in range(n_qubits)]
-shared_bases = [random.randint(0, 1) for _ in range(n_qubits)]
+#envoi de données
+data_to_send = {
+    "session_id": datetime.now().strftime("%Y%m%d_%H%M%S"),
+    "n_qubits": 4,
+    "qasm": qasm_circuit,
+    "timestamp": datetime.now().isoformat()
+    }
+file_send ="ref_circuit_param.json"
+with open(file_send, "w") as f:
+    json.dump(data_to_send, f, indent=2)
 
-qc = QuantumCircuit(n_qubits) #taille du circuit peut etre dynamique plus tard
+#alice crée une copie de la preparation (et non des etats quantiques) puis mesure de son coté
+ref_copy = ref_qc.copy()
+ref_copy.measure(range(4), range(4))
+sim = AerSimulator()
+compiled = transpile(ref_copy, sim)
+result = sim.run(compiled, shots=1024).result()
+counts = result.get_counts()
+print(counts)
 
-# generation de la même serie de qubits et de bases
-for i in range(n_qubits):
-    if shared_bits[i] == 1:
-        qc.x(i)                     # si le bit =1 alors appliquer flip gate au qubit |0>
+# envoi a travers sockets
+HOST = '127.0.0.1'
+PORT = 5555
 
-    if shared_bases[i] == 1:         # si base = 1 appliquer hadamard gate au qubit
-        qc.h(i)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    print(f"Alice en écoute sur {HOST}:{PORT} ... En attente de Bob")
 
+    conn, addr = s.accept()
+    print(f"Bob connecté depuis {addr}")
 
-# --- generation du fichier txt et le supprimer plus tard pour supprimer la trace
-print(tmps_creation + "\n") # juste pour test
-circuit_data = {
-    "session_id": id_session,
-    "timestamp": tmps_creation,
-    "protocol": "BB84",
-    "n_qubits": n_qubits,
-    "shared_bits": shared_bits,
-    "shared_bases": shared_bases,
-    "circuit_qasm": dumps(qc)   #traduction de l'etat du circuit en langage qasm
-}
+    import pickle
+    conn.sendall(json.dumps(data_to_send).encode())
+    print("  Reference circuit envoyé avec succès à Bob")
 
-fich_plain = f"circuit_data_{id_session}.json"
-with open(fich_plain, "w") as f:
-    json.dump(circuit_data, f, indent=2)
+    conn.close()
 
-# --- Generatiion des 2 fichiers encryptés (ssl pour l'instant puis pqc plus tard)
-#lire les 2 clés
-with open("Alice_pubK.pem", "rb") as f:
-    alice_public_key = serialization.load_pem_public_key(f.read())
-with open("Bob_pubK.pem", "rb") as f:
-    bob_public_key = serialization.load_pem_public_key(f.read())
-
-with open(fich_plain, "rb") as f:
-    plaintext_data = f.read()
-
-#generer une clé aes et chiffrer le file avec (classique pour l'instant)
-symmetric_key = Fernet.generate_key()
-cipher = Fernet(symmetric_key)
-encrypted_data = cipher.encrypt(plaintext_data)
-
-encrypted_file = f"circuit_encrypted_{id_session}.bin"
-with open(encrypted_file, "wb") as f:
-    f.write(encrypted_data)
-
-#chiffrage de la clé avec les clés pub de a et b
-alice_encrypted_key = alice_public_key.encrypt(
-    symmetric_key,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
-
-with open(f"alice_symkey_{id_session}.bin", "wb") as f:
-    f.write(alice_encrypted_key)
-bob_encrypted_key = bob_public_key.encrypt(
-    symmetric_key,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
-
-with open(f"bob_symkey_{id_session}.bin", "wb") as f:
-    f.write(bob_encrypted_key)
-
-os.remove(fich_plain)
-print("Plaintext supprimé.")
