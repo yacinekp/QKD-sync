95554e3ef9941776175db19dc383425b8318ae8d
yacinekp <71157269+yacinekp@users.noreply.github.com>
2026-05-26 12:44:44 +0100

    ajout de parametres de callibration

--- circuit_generator.py
+++ circuit_generator.py
@@ -5,6 +5,8 @@ from qiskit import transpile
 import socket
 from datetime import datetime
 import json
+import time
+import random
 
 #la fonction de generation du circuit de ref
 def build_quantum_circuit():
@@ -16,15 +18,32 @@ def build_quantum_circuit():
     qc.h(3) # qc3 = |->
     return qc
 
-#alice genere ce circuit :
+#alice genere ce circuit et mesurre le temps de generation:
+gen_debut = time.time()
 ref_qc = build_quantum_circuit()
+gen_fin = time.time()
+tps_gen = gen_fin - gen_debut
+
+#serialisation en QASM
 qasm_circuit = dumps(ref_qc)
+
+#parametres de synchronisation simulés
+decalage_phase = random.uniform(-0.05, 0.05)
+decalage_horloge = random.uniform(-0.001, 0.001)
+bruit_canal = random.uniform(0.0, 0.03)
+shots = 1024
 
 #envoi de données
 data_to_send = {
     "session_id": datetime.now().strftime("%Y%m%d_%H%M%S"),
     "n_qubits": 4,
+    "shots" : shots,
     "qasm": qasm_circuit,
+    "temps_generation_circuit" : tps_gen,
+    "decalage_horloge" : decalage_horloge,
+    "decalage_phase" : decalage_phase,
+    "nb_essais" : shots,
+    "bruit_canal" : bruit_canal,
     "timestamp": datetime.now().isoformat()
     }
 file_send ="ref_circuit_param.json"
@@ -35,16 +54,33 @@ with open(file_send, "w") as f:
 ref_copy = ref_qc.copy()
 ref_copy.measure(range(4), range(4))
 sim = AerSimulator()
-compiled = transpile(ref_copy, sim)
-result = sim.run(compiled, shots=1024).result()
-counts = result.get_counts()
-print(counts)
 
+#tps compilation
+compil_deb = time.time()
+compiled = transpile(ref_copy, sim)
+compil_fin =time.time()
+
+#tps execution
+exec_deb = time.time()
+result = sim.run(compiled, shots=1024).result()
+exec_fin = time.time()
+
+counts = result.get_counts()
+
+#affichage des parametres chez alice
+print("\n \033[1;33m Counts :", counts,' \033[0m\n')
+print(f"Temps génération circuit : {tps_gen:.6f} sec")
+print(f"Temps compilation : {compil_fin - compil_deb:.6f} sec")
+print(f"Temps exécution : {exec_fin - exec_deb:.6f} sec")
+print(f"décalage de phase simulé : {decalage_phase:.6f}")
+print(f"Décalage d'horloge simulé : {decalage_horloge:.6f} sec")
+print(f"Bruit estimé simulé: {bruit_canal:.6f} \n")
 # envoi a travers sockets
 HOST = '127.0.0.1'
 PORT = 5555
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
     print(f"Alice en écoute sur {HOST}:{PORT} ... En attente de Bob")
--- circuit_recieve.py
+++ circuit_recieve.py
@@ -17,10 +17,10 @@ payload = json.loads(data.decode())
 # 2. rebuild circuit
 qc = loads(payload["qasm"])
 
-# 3. measurement
+# 3. measure
 qc.measure(range(qc.num_qubits), range(qc.num_clbits))
 
-# 4. simulate
+# 4. simulation
 sim = AerSimulator()
 compiled = transpile(qc, sim)
 
@@ -28,4 +28,14 @@ result = sim.run(compiled, shots=1024).result()
 
 bob_counts = result.get_counts(compiled)
 
-print("Bob counts:", bob_counts)
+print("\n \033[1;33m Bob counts:", bob_counts, "\033[0m \n")
+print("PARAMÈTRES REÇUS \n")
+print(f"Session ID : {payload['session_id']}")
+print(f"Timestamp : {payload['timestamp']}")
+print(f"Nombre de qubits : {payload['n_qubits']}")
+print(f"Nombre de shots : {payload['shots']}")
+print(f"Temps génération circuit : "f"{payload['temps_generation_circuit']:.6f} sec")
+print(f"Phase drift : "f"{payload['decalage_phase']:.6f}")
+print(f"Décalage horloge : "f"{payload['decalage_horloge']:.6f} sec")
+print(f"Bruit estimé : "f"{payload['bruit_canal']:.6f}")
+
